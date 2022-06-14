5ad1294a7bbc28e8687865ea1a7709c91c9c0abd
ishinu24 <shubham@theyaksgroup.com>
2022-06-14 10:15:17 +0530

    2. Settings Part2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-let age: number = 20;
-if  (age<50) 
-    age+=10;
-console.log(age)
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level;
+let a = 20;
+let numbers : number[] = [1,2,3];
+
+function calculateTax(income: number, taxYear: number): number { 
+    if(taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+    //Js by defaul returns undefined, so in case if 'if' condition is not true, it returns 'not number'
+}
